b726578b099b196add18fa4bfb41a7211e77738f
Hernaval <hernaval.devmg@gmail.com>
2022-06-16 12:45:21 +0300

    welcome page slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,159 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:scratch_app/pages/bar_item_page.dart';
+import 'package:scratch_app/pages/home_page.dart';
+import 'package:scratch_app/pages/my_page.dart';
+import 'package:scratch_app/pages/search_page.dart';
+import 'package:scratch_app/pages/welcome_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  List pages = [
+      HomePage(),
+      BarItemPage(),
+      SearchPage(),
+      MyPage()
+  ];
+  int currentIndex= 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter demo",
       theme: ThemeData(
-        primarySwatch: Colors.green
+        primarySwatch: Colors.blue
       ),
-      home: const HomePage()
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String value = "Flutter scratch app";
-  List<BottomNavigationBarItem> _item = [];
-  List article = List<String>.generate(25, (index) => "Article $index");
-
-
-
-  void onClick() {
-    setState(() {
-      value = "Thanks for clicking";
-    });
-  }
-
-  void onClickTwo() {
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Container(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Bonjour les amis !")
-              ],
-            ),
-          );
-        }
-    );
-  }
-
-  Future showAlert() async {
-    await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            title: Text("Are you bored"),
-            children: [
-              SimpleDialogOption(
-                child: Text("OUI", style: TextStyle(color: Colors.blue),),
-                onPressed: () {},
-              )
-            ],
-          );
-        }
-        );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _item.add(BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.grey), label: "Home"));
-    _item.add(BottomNavigationBarItem(icon: Icon(Icons.favorite, color: Colors.grey), label: "Home"));
-    _item.add(BottomNavigationBarItem(icon: Icon(Icons.search, color: Colors.grey), label: "Home"));
-    _item.add(BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle, color: Colors.grey), label: "Home"));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(value),
-          actions: const [
-            Icon(Icons.notification_add_sharp, size: 30),
-            Icon(Icons.download, size: 30)
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black54,
+          unselectedItemColor: Colors.grey.withOpacity(0.5),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 0,
+          onTap: onTap,
+          currentIndex: currentIndex,
+          items: [
+            BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+            BottomNavigationBarItem(label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+            BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+            BottomNavigationBarItem(label: "My",icon: Icon(Icons.person)),
           ],
         ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
-          icon: const Icon(Icons.add),
-          label: Text("Create a task"),
-        ),
-        // persistentFooterButtons: const [
-        //   Icon(Icons.home, size: 25),
-        //   Icon(Icons.favorite, size: 25),
-        //   Icon(Icons.search, size: 25),
-        // ],
-        bottomNavigationBar: BottomNavigationBar(
-          items: _item
-        ),
-        body: PageView.builder(
-            itemBuilder: (BuildContext context, int index) =>
-              PageWidget(entry: pages[index])
-            ,
-            itemCount: pages.length,
-        )
-    );
-  }
-}
-
-class PageInfo {
-  final String title;
-  final String image;
-  final String description;
-
-  const PageInfo(this.title, this.image, this.description);
-
-}
-
-final List<PageInfo> pages= [
-  PageInfo("Introduction", "images/1.png", "This is a sample introduction images"),
-  PageInfo("Bacis", "images/2.jpg", "This is a sample thing you must know"),
-  PageInfo("Deep", "images/3.jpg", "Go deeper with this section")
-];
-
-class PageWidget extends StatefulWidget {
-  final PageInfo entry;
-  const PageWidget({Key? key, required this.entry}) : super(key: key);
-  @override
-  _PageWidgetState createState() => _PageWidgetState();
-}
-
-class _PageWidgetState extends State<PageWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(10),
-    child: ListView(
-        children: [
-          Row(
-            children: [
-              Text(widget.entry.title, style: TextStyle(color: Colors.blue, fontSize: 25))
-            ],
-          ),
-          SizedBox(height: 10),
-          Image.asset(widget.entry.image, fit: BoxFit.cover,),
-          SizedBox(height: 10),
-          Text(widget.entry.description, style: TextStyle(color: Colors.blue, fontSize: 15))
-        ],
-    ),
+        body: pages[currentIndex],
+      )
     );
   }
 }
---
+++ lib/widgets/responsive_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:scratch_app/misc/colors.dart';
+
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+  ResponsiveButton({Key? key, this.isResponsive = false, this.width}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("images/button-one.png")
+        ],
+      ),
+    );
+  }
+}
